d511bd3a5265fe9ae4960314f2472be1c01cf0bd
dvadher <divya@alchemytech.ca>
2022-01-13 01:39:26 +0530

    all pages

--- js/custom.js
+++ js/custom.js
@@ -70,11 +70,10 @@ $(document).ready(function() {
 
 
 	$('#services_slider').owlCarousel({
-        loop: true,
-        center: true,
+        loop: true,	    
         items: 3,
         animateOut: 'fadeOut',
-		autoplay: true,
+		autoplay: false,
         dots: false,
         nav: false,
         autoplayTimeout: 4000,
@@ -99,32 +98,112 @@ $(document).ready(function() {
 		}
 		});
 	});
+
+
+	
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav',
+		   autoplay: false,
+	  });
+	  $('.slider-nav').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: false,
+		centerMode: false,
+		focusOnSelect: true,
+		autoplay:false,
+		prevArrow: ' <i class="fa fa-chevron-left" aria-hidden="true"></i>',
+    	nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+	  });
+			  
 	
 
 /*====================== filter-slider end ====================== */
 
 /*====================== js for scrollTop ====================== */
 
-	// Add smooth scrolling to all links
-	// $(".navbar a").on('click', function(event) {
-	// 	var _href = $(this).attr('href');
-	// 	// Make sure this.hash has a value before overriding default behavior
-	// 	if (_href !== "") {
-	// 		// Prevent default anchor click behavior
-	// 		event.preventDefault();
+	// // Add smooth scrolling to all links
+	$(".nav_jump").on('click', function(event) {
+		var _href = $(this).attr('href');
+		// Make sure this.hash has a value before overriding default behavior
+		if (_href !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-	// 		$('html, body').animate({
-	// 			scrollTop: $(_href).offset().top
-	// 		}, 800, function() {});
-	// 	} // End if
-	// });
-
-	$(document).on('click', '.filter-btn', function() {
-		$('.filter').toggleClass('filter-in');
+			$('html, body').animate({
+				scrollTop: $(_href).offset().top
+			}, 800, function() {});
+		} // End if
 	});
+
+	var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ff908369}";
+        document.body.appendChild(css);
+    };
 
 	AOS.init({
 		duration: 1200,
-	})
+	});
+
+	
 
 }); /*====================== all js end ====================== */
